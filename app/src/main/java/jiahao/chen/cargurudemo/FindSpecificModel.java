411cb599f578fd50cd1944ab7584a6290fdde44d
Luka <necajev20@gmail.com>
2021-03-06 15:48:51 -0500

    Trying to get the models to display from the database.

--- app/src/main/java/jiahao/chen/cargurudemo/FindSpecificModel.java
+++ app/src/main/java/jiahao/chen/cargurudemo/FindSpecificModel.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,7 +22,8 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class FindSpecificModel extends AppCompatActivity {
-
+    //Creating the context
+    Context context;
     //List of carModel Objects so I can pass the one they choose to the users.
     ArrayList<CarModel> vehicleList;
     //Creating a List to hold all of the Names of the Make
@@ -30,36 +32,44 @@ public class FindSpecificModel extends AppCompatActivity {
     ArrayList<String> modelList;
     //Creating a List to hold all of the Names of the Years
     ArrayList<String> yearList;
+    //Getting the Spinner(Combobox) Objects
+    Spinner makeSpinner;
+    Spinner modelSpinner;
+    Spinner yearSpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_specific_model_list);
-
-    }
-    /*
-     * This Function is created to get all of the Cars Makes Models and years, and put them in the
-     * respective ComboBox's (Spinners). This will populate the public arrays from the class
-     * (1. makeList, 2.  modelList, 3. YearList)
-     * The selected vehicle information will be passed to the next class.
-     */
-
-    public ArrayList<CarModel> getMakeModelYear() {
-        TextView testingTV = findViewById(R.id.testingResultTV);
+        //Setting the context
+        context = getApplicationContext();
+        //vehicleList = getMakeModelYear();
         //getting all of the items from the Vehicle portion of the database.
-        DatabaseReference listOfVehicles = FirebaseDatabase.getInstance().getReference().child("Vehicle");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Vehicle");
 
         //List of carModel Objects so I can pass the one they choose to the users.
         vehicleList = new ArrayList<>();
-        //Creating a List to hold all of the Names of the Make
-        makeList = new ArrayList<>();
-        //Creating a List to hold all of the Names of the Models
-        modelList = new ArrayList<>();
-        //Creating a List to hold all of the Names of the Years
-        yearList = new ArrayList<>();
+//        //Creating a List to hold all of the Names of the Make
+        ArrayList<String> makeList = new ArrayList<>();
+//        //Creating a List to hold all of the Names of the Models
+        ArrayList<String> modelList = new ArrayList<>();
+//        //Creating a List to hold all of the Names of the Years
+//        yearList = new ArrayList<>();
 
-        listOfVehicles.addValueEventListener(new ValueEventListener() {
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                /*
+                 *   This method is used to get the vehicle information from the database.
+                 *   The Information is stored into the VehicleList. This list will contain the make,
+                 *   model, year and category. This will be used in the onCreate method to look through
+                 *   the information and populate the Spinners(ComboBoxes)
+                 *
+                 *   Will Populate:
+                 *       ArrayList<String> makeList ()
+                 *   Will Return:
+                 *       ArrayList <carModel> vehicleList (**only make, model, year and category are filled out**)
+                 */
                 String category ="";
                 String make = "";
                 String model = "";
@@ -70,64 +80,48 @@ public class FindSpecificModel extends AppCompatActivity {
                     //Get the makes list Once they get this list then populate the others
                     //for each make in the Given Category (Category is passed)
                     for (DataSnapshot aMake : dataSnapshot.child(category).getChildren()) {
-                        //setting the make so we can iterate trhough them
+                        //Checking if the Make is already in the list of makes
+                        if (!makeList.contains(make)){
+                            //if it is not in the list add it
+                            makeList.add(model);
+                        }
+                        //setting the make so we can iterate through them
                         make = aMake.getKey();
-                        //Add the make to the list of makes
-                        makeList.add(make);
-
                         //Go through each year and add the models for that year
                         for (DataSnapshot aYear : dataSnapshot.child(make).getChildren()) {
                             //setting the year so we can iterate through each year and get the models
                             year = aYear.getKey();
-                            //Add the year to the list of years
-                            yearList.add(year);
                             //Go through each model of each make
                             for (DataSnapshot aModel : dataSnapshot.child(year).getChildren()) {
                                 CarModel carModel = new CarModel();
+                                //Getting the model
+                                model = aModel.getKey();
                                 //Adding the make to the carModel Object
                                 carModel.Make = make;
                                 //Adding the category to the carModel Object
                                 carModel.Category = category;
                                 //add the models name to the carModel Object
-                                carModel.Model = aModel.getKey();
+                                carModel.Model = model;
                                 //Get the year and add it to the carModel Object
                                 carModel.Year = year;
-                                //Adding the carModel to the carModel List
-                                vehicleList.add(carModel);
+                                //Check if the model is already in the models array list
+                                if (!modelList.contains(model)){
+                                    //if it is not in the list add it
+                                    modelList.add(model);
+                                    //Adding the carModel to the carModel List
+                                    vehicleList.add(carModel);
+                                }
+
+                                //otherwise do not add the model as it already exists.
+
                                 //Do I do one more loop to get the next information?
                                 //Test both but first try just the first one.
                             }
                         }
-
-
-
-                        //Getting the Category from the vehicle
-
-                        //This for loop will go through each make and add the make
-
-                        //Add the car to the Make array.
-
-                        //If the vehicle is already in the array dont add it.
-
-                        //Once we have the arrays, populate the spinner when the user clicks make, only show the models with that make.
-
-                        //Once the user chooses models, only show the model that the user is going to make.
                     }
-
                 }
-
-                ArrayAdapter spinnerAdapter;
-
-                //listview
-                ListView lv = findViewById(R.id.resultListView);
-                if (carType.equals("Commuter")) {
-                    spinnerAdapter = new ArrayAdapter<>(SortingTop3.this, android.R.layout.simple_list_item_1, carCommuterString);
-                } else {
-                    spinnerAdapter = new ArrayAdapter<>(SortingTop3.this, android.R.layout.simple_list_item_1, carSportString);
-                }
-                // set the list object into spinner object
-                lv.setAdapter(spinnerAdapter);
-
+                //Clearing the modelList
+                modelList.clear();
             }
 
             @Override
@@ -136,18 +130,129 @@ public class FindSpecificModel extends AppCompatActivity {
             }
         });
 
-        return null;
+
+        //Creating the Spinner Adapters
+        ArrayAdapter makeSpinnerAdapter;
+        ArrayAdapter modelSpinnerAdapter;
+        ArrayAdapter yearSpinnerAdapter;
+
+        //Creating the Spinner Objects
+        Spinner makeSpinner = findViewById(R.id.spMake);
+        Spinner modelSpinner = findViewById(R.id.spModel);
+        Spinner yearSpinner = findViewById(R.id.spYear);
+
+        //Set the make spinner list when the user visits the page.
+
+        makeSpinnerAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, makeList);
+//        modelSpinnerAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, carSportString);
+//        yearSpinnerAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, carSportString);
+
+        //Creating the Make Spinner List
+        makeSpinner.setAdapter(makeSpinnerAdapter);
+
+
+        // set the list object into spinner object
+        //modelSpinner.setAdapter(spinnerAdapter);
+        //yearSpinner.setAdapter(spinnerAdapter);
+
+
     }
+    /*
+     * This Function is created to get all of the Cars Makes Models and years, and put them in the
+     * respective ComboBox's (Spinners). This will populate the public arrays from the class
+     * (1. makeList, 2.  modelList, 3. YearList)
+     * The selected vehicle information will be passed to the next class.
+     */
 
+    public ArrayList<CarModel> getMakeModelYear() {
+        //getting all of the items from the Vehicle portion of the database.
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Vehicle");
 
+        //List of carModel Objects so I can pass the one they choose to the users.
+        vehicleList = new ArrayList<>();
+//        //Creating a List to hold all of the Names of the Make
+        ArrayList<String> makeList = new ArrayList<>();
+//        //Creating a List to hold all of the Names of the Models
+         ArrayList<String> modelList = new ArrayList<>();
+//        //Creating a List to hold all of the Names of the Years
+//        yearList = new ArrayList<>();
 
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                /*
+                *   This method is used to get the vehicle information from the database.
+                *   The Information is stored into the VehicleList. This list will contain the make,
+                *   model, year and category. This will be used in the onCreate method to look through
+                *   the information and populate the Spinners(ComboBoxes)
+                *
+                *   Will Populate:
+                *       ArrayList<String> makeList ()
+                *   Will Return:
+                *       ArrayList <carModel> vehicleList (**only make, model, year and category are filled out**)
+                 */
+                String category ="";
+                String make = "";
+                String model = "";
+                String year = "";
 
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    category = snapshot.getKey();
+                    //Get the makes list Once they get this list then populate the others
+                    //for each make in the Given Category (Category is passed)
+                    for (DataSnapshot aMake : dataSnapshot.child(category).getChildren()) {
+                        //Checking if the Make is already in the list of makes
+                        if (!makeList.contains(make)){
+                            //if it is not in the list add it
+                            makeList.add(model);
+                        }
+                        //setting the make so we can iterate through them
+                        make = aMake.getKey();
+                        //Go through each year and add the models for that year
+                        for (DataSnapshot aYear : dataSnapshot.child(make).getChildren()) {
+                            //setting the year so we can iterate through each year and get the models
+                            year = aYear.getKey();
+                            //Go through each model of each make
+                            for (DataSnapshot aModel : dataSnapshot.child(year).getChildren()) {
+                                CarModel carModel = new CarModel();
+                                //Getting the model
+                                model = aModel.getKey();
+                                //Adding the make to the carModel Object
+                                carModel.Make = make;
+                                //Adding the category to the carModel Object
+                                carModel.Category = category;
+                                //add the models name to the carModel Object
+                                carModel.Model = model;
+                                //Get the year and add it to the carModel Object
+                                carModel.Year = year;
+                                //Check if the model is already in the models array list
+                                if (!modelList.contains(model)){
+                                    //if it is not in the list add it
+                                    modelList.add(model);
+                                    //Adding the carModel to the carModel List
+                                    vehicleList.add(carModel);
+                                }
 
+                                //otherwise do not add the model as it already exists.
 
+                                //Do I do one more loop to get the next information?
+                                //Test both but first try just the first one.
+                            }
+                        }
+                    }
+                }
+                //Clearing the modelList
+                modelList.clear();
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
 
-
+        return vehicleList;
+    }
     /*
     *  This Event Handler is fired when the user clicks the Search button on the Find Specific Car Page.
     *  This function will Send the user to the specific cars page and will pass the Specific Vehicle
@@ -164,5 +269,31 @@ public class FindSpecificModel extends AppCompatActivity {
         Context context = getApplicationContext();
         Intent intent = new Intent(context, SpecificModel.class);
         startActivity(intent);
+    }
+    /*
+     * This Event Handler is paired so when the user clicks any of the spinners it will check if a
+     * value is chosen.
+     *
+     * The Function is created to populate The Model and View based on what value is returned from the Make.
+     * If the user chooses a make, this function will be called to populate the make, once that file is populated,
+     * the function will populate the years that are available
+     *
+     * This function will return None
+     */
+    public void onClickSpinners(View view){
+        //Check to see if user has chosen a make.
+        String make = "";
+         make = makeSpinner.getSelectedItem().toString();
+        if (make.equals("")){
+            //look for the models of the make
+            for (CarModel car : vehicleList){
+                //if the users make is the make of the user, display the cars with the make
+                if (car.Make.equals(make)){
+                    //add the car into the list of models to display.
+                    modelList.add(car.getModel());
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/jiahao/chen/cargurudemo/HomePage.java
+++ app/src/main/java/jiahao/chen/cargurudemo/HomePage.java
@@ -10,29 +10,28 @@ import androidx.appcompat.app.AppCompatActivity;
 public class HomePage extends AppCompatActivity {
 //Footer menu implementation
 //https://material.io/components/bottom-navigation/android#using-bottom-navigation
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        context = getApplicationContext();
 
     }
 
 // Find a car Link
     public void onClickToGuru (View view){
-        Context context = getApplicationContext();
         Intent intent = new Intent(context, FindACarForYou.class);
         startActivity(intent);
     }
 // Find a Specific Model Link
     public void onClickToSpecificModel (View view){
-        Context context = getApplicationContext();
         Intent intent = new Intent(context, FindSpecificModel.class);
         startActivity(intent);
     }
 
 // Browse By Category Link
     public void onClickToCategories (View view){
-        Context context = getApplicationContext();
         Intent intent = new Intent(context, BrowseByCategory.class);
         startActivity(intent);
     }
@@ -42,13 +41,11 @@ public class HomePage extends AppCompatActivity {
 //    }
 //Settings Link
 public void onClickToSettings (View view){
-    Context context = getApplicationContext();
     Intent intent = new Intent(context, Settings.class);
     startActivity(intent);
 }
 // Home Page Link
 public void onClickToHomePage (View view){
-    Context context = getApplicationContext();
     Intent intent = new Intent(context, FindACarForYou.class);
     startActivity(intent);
 }
