1f86c835611b6ee0fca39f026103cc74d835c015
Luka <necajev20@gmail.com>
2021-03-30 01:21:05 -0400

    Adding The Question Class needed to store the data

---
+++ app/src/main/java/jiahao/chen/cargurudemo/Question.java
@@ -0,0 +1,11 @@
+package jiahao.chen.cargurudemo;
+
+import java.util.List;
+
+public class Question {
+    //Question includes: String question;, List<Answers>Answers;, String Value;
+    public String question;
+    public String value;
+    public List<String> answers;
+
+}
