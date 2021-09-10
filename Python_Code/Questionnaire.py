79b36476dfa1eb33cebee894b37dfbe54b3add57
Luka1468 <necajevl@sheridancollege.ca>
2021-09-10 11:27:15 -0400

    Julies Updated Python Questionnaire Algorithm

--- Python_Code/Questionnaire.py
+++ Python_Code/Questionnaire.py
@@ -9,79 +9,67 @@ firebase_admin.initialize_app(cred,{"databaseURL":"https://car-guru-demo-default
 
 #get parent branches references 
 questionRef = db.reference("Questions").get()
-vehicleRef = db.reference("TestBranch").get()
+vehicleRef = db.reference("Vehicle").get()
 
 #initialize
-commuterCount = 0
-sportCount = 0
 filterList = {}
 carList = []
 finalList = []
-#PART 1: get MAIN QUESTION branch reference
-mainQuestion = questionRef.get("MainQuestion")
 
-# Prompt questions and ask for answers:
-for index, (key, question) in enumerate(mainQuestion.items()):
-    #display question
-    question = mainQuestion.get(key)
-    questionDescription = question.get("Description")
-    print("Question: %s " % ( questionDescription))
+#category key-value dict
+categoriesNumberDict = {0: "Commuter", 1: "Sport", 2: "Utility"}
 
-    #display answers
-    questionAnswers = question.get("Answers")
-    for key, answer in questionAnswers.items():
-        print(str(key[4:]) + ". " + answer["Description"])
+categoriesInfoDict = {  0 : {"Name": "Commuter", "QuestionBranchName": "CommuterQuestion", "Prefix" : "cqa", "Count" : 0},
+                        1 : {"Name": "Sport", "QuestionBranchName": "SportQuestion", "Prefix" : "sqa", "Count" : 0}     }
 
-    #get user input
-    answer = "mqa" + str(index + 1) + input("\n")
+def getQuestionsAndAnswersToDisplay(typeOfQuestions, prefix, main):
+    
+    #get reference from database
+    questionListsRef = questionRef.get(typeOfQuestions)
+    
+    # Prompt questions and ask for answers:
+    for index, (key, question) in enumerate(questionListsRef.items()):
+        #display question
+        question = questionListsRef.get(key)
+        questionDescription = question.get("Description")
+        print("Question: %s " % ( questionDescription))
 
-    #handle user choices
-        # Used for questions that are String not Lists
-    if (questionAnswers[answer]["Value"] == "Commuter"):
-        commuterCount = commuterCount + 1
-    elif (questionAnswers[answer]["Value"] == "Sport" ):
-        sportCount = sportCount + 1
-        # Used for questions that are Lists not String
-    elif ("Sport" in questionAnswers[answer]["Value"]):
-        sportCount = sportCount + 1
-    elif ("Commuter" in questionAnswers[answer]["Value"]):
-        commuterCount = commuterCount + 1
-        #Used for question 5
-    elif (questionAnswers[answer]["Value"] == str(2)):
-        sportCount = sportCount + 1
-        filterList["Seats"] = "2"
-    else:
-        commuterCount = commuterCount + 1
-        filterList["Seats"] = questionAnswers[answer]["Value"]
+        #display answers
+        questionAnswers = question.get("Answers")
+        for key, answer in questionAnswers.items():
+            print(str(key[4:]) + ". " + answer["Description"])
+
+        #get user input
+        answer = prefix + str(index + 1) + input("\n")
+        
+        if (main):
+            handleMainQuestionsInputs(questionAnswers, answer)
+        else:
+            handleOtherTypesInputs(questionAnswers, answer, question)
+    
+    #reset
+    maxCount = 0
+    returnList = {}
+    
+    # return category with the most count
+    if (main):
+        for key, value in categoriesInfoDict.items():
+            if (categoriesInfoDict[key]["Count"] > maxCount):
+                returnList = categoriesInfoDict[key]
+    
+    return returnList
+        
+def handleMainQuestionsInputs(questionAnswers, answer):
+    
+    for key, value in categoriesInfoDict.items():
+        if (questionAnswers[answer]["Value"] == categoriesInfoDict[key]["Name"] or categoriesInfoDict[key]["Name"] in questionAnswers[answer]["Value"]):
+            categoriesInfoDict[key]["Count"] +=1  
 
     #Print out results
-    print("Commuter: %s "  % commuterCount) 
-    print("Sport: %s" % sportCount)
-
-#PART 2: get specific CATEGORY QUESTION branch reference
-if (commuterCount > sportCount):
-    categoryQuestion = questionRef.get("CommuterQuestion")
-    categoryChosen = "Commuter"
-    prefix = "cqa"
-else:
-    categoryQuestion = questionRef.get("SportQuestion")
-    categoryChosen = "Sport"
-    prefix = "sqa"
-
-#Prompt questions and ask for answers:
-for index, (key, answer) in enumerate(categoryQuestion.items()):
-    # display question
-    question = categoryQuestion.get(key)
-    questionDescription = question.get("Description")
-    print("Question: %s " % ( questionDescription))
-
-    # display answers
-    questionAnswers = question.get("Answers")
-    for key, answer in questionAnswers.items():
-        print(str(key[4:]) + ". " + answer["Description"])
-
-    answer = prefix + str(index+1) + input("\n")
-
+    for key, value in categoriesInfoDict.items():
+        print(categoriesInfoDict[key]["Name"] + " : %s "  % categoriesInfoDict[key]["Count"])
+        
+def handleOtherTypesInputs(questionAnswers, answer, question):
     # get DBField and Value from the answer given by user
     dbField = question.get("DBField")
     answerValue = questionAnswers[answer]["Value"]
@@ -89,60 +77,80 @@ for index, (key, answer) in enumerate(categoryQuestion.items()):
     #append filter to filterList
     filterList[dbField] = answerValue
 
-print(filterList)
+def sortThroughCarList(categoryChosen):
+    #Creating Lists for adding and removing 
+    smallerList = set()
+    greaterList = set()
+    smallerThanList = set()
+    greaterThanList = set()
+    allList = set()
+    normalList = set()
+    yearList = set()
 
-#Debug
-# filterList = {'Year': '2016', 'Weight': '>2000', 'Convertible': 'Yes', 'GroundClearance': '5', 'Cylinders': '<=6'}
-# categoryChosen = "Sport"
+    for makeKey, make in vehicleRef.items():
+        for modelKey, model in make.items():
+            for trimKey, trim in model.items():
+                for yearKey, year in trim.items():
+                    if (str(year["Category"]) == categoryChosen):
+                        #Key/ Value being {'Year': 'All', 'Weight': '<2000'...}
+                        for key,val in filterList.items():
+                            #Import printable to allow this.
+                            #Checking if there are special characters
+                            if (key == "Year"):
+                                comparedValue = yearKey
+                            else:
+                                comparedValue = year[key] 
 
-#Creating Lists for adding and removing 
-smallerList = set()
-greaterList = set()
-smallerThanList = set()
-greaterThanList = set()
-allList = set()
-normalList = set()
-yearList = set()
-
-for makeKey, make in vehicleRef.items():
-    for modelKey, model in make.items():
-        for trimKey, trim in model.items():
-            for yearKey, year in trim.items():
-                if (str(year["Category"]) == categoryChosen):
-                    #Key/ Value being {'Year': 'All', 'Weight': '<2000'...}
-                    for key,val in filterList.items():
-                        #Import printable to allow this.
-                        #Checking if there are special characters
-                        if (key == "Year"):
-                            comparedValue = yearKey
-                        else:
-                            comparedValue = year[key] 
-
-                        if ("<=" in val):
-                            if(int(comparedValue) <= int(val[2:])):
-                                smallerThanList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
-                        elif (">=" in val):  
-                            if(int(comparedValue) >= int(val[2:])):
-                                greaterThanList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
-                        elif ("<" in val):
-                            if(int(comparedValue) < int(val[1:])):
-                                smallerList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
-                        elif (">" in val):  
-                            if(int(comparedValue) > int(val[1:])):
-                                greaterList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
-                        elif ("-" in val):
-                            if(int(comparedValue) >= int(val.split("-",1)[0]) and int(comparedValue) <= int(val.split("-",1)[1])):
+                            if ("<=" in val):
+                                if(int(comparedValue) <= int(val[2:])):
+                                    smallerThanList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
+                            elif (">=" in val):  
+                                if(int(comparedValue) >= int(val[2:])):
+                                    greaterThanList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
+                            elif ("<" in val):
+                                if(int(comparedValue) < int(val[1:])):
+                                    smallerList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
+                            elif (">" in val):  
+                                if(int(comparedValue) > int(val[1:])):
+                                    greaterList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
+                            elif ("-" in val):
+                                if(int(comparedValue) >= int(val.split("-",1)[0]) and int(comparedValue) <= int(val.split("-",1)[1])):
+                                    allList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
+                            elif (val == "All"):
                                 allList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
-                        elif (val == "All"):
-                            allList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
-                        else:
-                            if(comparedValue == val):
-                                normalList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
+                            else:
+                                if(comparedValue == val or comparedValue == "Both"):
+                                    normalList.add("%s %s %s %s" %(makeKey, modelKey, trimKey, yearKey))
+    
+    list_of_sets = [smallerList, greaterList, smallerThanList, greaterThanList, allList, normalList, yearList]
+    return list_of_sets
 
-list_of_sets = [smallerList, greaterList, smallerThanList, greaterThanList, allList, normalList, yearList]
+def printOutListOfCarsReturned(list_of_sets):
+    # Empty sets evaluate to false,
+    # so will be excluded from list comp.
+    non_empties = [x for x in list_of_sets if x]
+    solution_set = set.intersection(*non_empties)                   
+    #print(list(solution_set))
+    for car in list(solution_set):
+        print(car)
+        
+def main():
+    
+    for key, value in categoriesInfoDict.items():
+        categoriesInfoDict[key]["Count"] = 0
+    
+    #PART 1: get MAIN QUESTION branch reference
+    categoryChosen = getQuestionsAndAnswersToDisplay("MainQuestion", "mqa", main=True)
+    
+    #PART 2: get specific CATEGORY QUESTION branch reference
+    getQuestionsAndAnswersToDisplay(categoryChosen["QuestionBranchName"], categoryChosen["Prefix"], main=False)
+    
+    print(filterList)
+    
+    listOfSets = sortThroughCarList(categoryChosen["Name"])
+    
+    printOutListOfCarsReturned(listOfSets)
 
-# Empty sets evaluate to false,
-# so will be excluded from list comp.
-non_empties = [x for x in list_of_sets if x]
-solution_set = set.intersection(*non_empties)                   
-print(list(solution_set))
+if __name__ == '__main__':
+    main()
+
