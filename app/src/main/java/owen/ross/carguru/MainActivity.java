9cde7c809c69f58b698189a1b0f884768f39551d
Luka1468 <necajevl@sheridancollege.ca>
2021-10-28 22:13:07 -0400

    Adding Settings Fragment and Creating a lighter theme. This is minimal Theme and does not change the background color

--- app/src/main/java/owen/ross/carguru/MainActivity.java
+++ app/src/main/java/owen/ross/carguru/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_search_car, R.id.navigation_questionnaire, R.id.navigation_list_of_cars_based_on_category)
+                R.id.navigation_home, R.id.navigation_search_car, R.id.navigation_questionnaire, R.id.navigation_my_garage, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
