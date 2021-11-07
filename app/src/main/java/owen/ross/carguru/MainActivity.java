ee2c2d8dbb444af71cdfd55f7ee4a76bb2511d0a
Luka1468 <necajevl@sheridancollege.ca>
2021-11-06 23:27:05 -0400

    Adding Splash Page!

--- app/src/main/java/owen/ross/carguru/MainActivity.java
+++ app/src/main/java/owen/ross/carguru/MainActivity.java
@@ -1,6 +1,8 @@
 package owen.ross.carguru;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
@@ -11,6 +13,10 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+/*
+ *
+ * This is the splash screen of our app.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,19 +24,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //Hides the Top Action Bar.
         getSupportActionBar().hide();
-
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_search_car, R.id.navigation_questionnaire, R.id.navigation_my_garage, R.id.navigation_settings)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
 
-        FirebaseApp.initializeApp(this);
+        Handler handler = new Handler();
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        // Navigate to the next page after 3 seconds
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(intent);
+            }
+        }, 3000);
+
+
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_search_car, R.id.navigation_questionnaire, R.id.navigation_my_garage, R.id.navigation_settings)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
+//
+//        FirebaseApp.initializeApp(this);
     }
 
 }
